ff8ea338ae213e1128f6e97d4ddc809fb435cf7d
Jian Han <jianhan@mail.com>
2018-07-15 22:51:34 +1000

    [feat] Install cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \Spatie\ResponseCache\Middlewares\CacheResponse::class,
+        \Spatie\Cors\Cors::class
     ];
 
     /**
