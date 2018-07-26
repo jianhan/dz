a455d3ffa712c34348ca3ab85948f5c005e2fc74
Jian Han <jianhan@mail.com>
2018-07-26 21:05:48 +1000

    [feat] Removed unwanted service provider

--- config/app.php
+++ config/app.php
@@ -178,7 +178,6 @@ return [
         // the driver for Elasticsearch
         Laravel\Scout\ScoutServiceProvider::class,
         ScoutElastic\ScoutElasticServiceProvider::class,
-        FractalServiceProvider::class,
     ],
 
     /*
